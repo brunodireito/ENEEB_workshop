2cfa45fec2ed3e272f8fb1155bde29a4586e1f72
brunodireito <migueldireito@gmail.com>
2021-03-06 16:29:58 +0000

    Update run_server_to_do.m

--- code_workshop/server/run_server_to_do.m
+++ code_workshop/server/run_server_to_do.m
@@ -13,47 +13,48 @@ bytearray=[];
 
 % Create OBJ server and initialize.
 % server=
-% server.
+% connected=server.
 %%
-server=Eneeb_server(host, port);
-server.initialize();
-try
-    
-    for i=1:200%size(TEST,1)
+
+if connected
+    try
         
-        % float2byte datatype
-        for f=1:length(TEST(:,i))
-            bytearray=[bytearray typecast(TEST(f,i),'uint8')];
+        for i=1:size(TEST,2)
+            
+            % float2byte datatype
+            for f=1:length(TEST(:,i))
+                bytearray=[bytearray typecast(TEST(f,i),'uint8')];
+            end
+            
+            % bytearray size = 8 (bytes per sample) * 41 elements - need to send
+            % 328 bytes per sample
+            
+            %% [TODO:] SEND MESSAGE (send message through server)
+            % hint: help Eneeb_server
+            
+            % server.
+            %%
+            
+            pause(.25)
+            
+            fprintf('[SERVER: ] Sample # %i sent.\n', i);
+            
+            bytearray=[];
         end
         
-        % bytearray size = 8 (bytes per sample) * 41 elements - need to send
-        % 328 bytes per sample
+        %% [TODO:] SEND MESSAGE informing that run ended.
+        % hint: help zeros, Eneeb_server
         
-        %% [TODO:] SEND MESSAGE (send message through server)
-        % hint: help Eneeb_server
-        
-        % server.
+        % server.;
         %%
-        server.sendmessage(bytearray); % to_do
-        pause(.25)
         
-        fprintf('[SERVER: ] Sample # %i sent.\n', i);
+        fprintf('[SERVER: ] Last sample sent.\n');
         
-        bytearray=[];
+        % Close server.
+        server.close();
+        
+    catch ME
+        server.close()
+        rethrow(ME)
     end
-    
-    %% [TODO:] SEND MESSAGE informing that run ended.
-    % hint: help zeros, Eneeb_server
-    
-    % server.;
-    %%
-    server.sendmessage(zeros(1,328)); % to_do
-    fprintf('[SERVER: ] Last sample sent.\n');
-    
-catch ME
-    server.close()
-    rethrow(ME)
 end
-
-% Close server.
-server.close();
